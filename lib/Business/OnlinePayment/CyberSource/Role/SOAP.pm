f737a926d7755a6bc1490ede2b2134a8b908f76a
Caleb Cushing <xenoterracide@gmail.com>
2011-08-01 10:52:33 -0500

    first attempt at an implemenation of the SOAP role
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Business/OnlinePayment/CyberSource/Role/SOAP.pm
+++ lib/Business/OnlinePayment/CyberSource/Role/SOAP.pm
@@ -16,6 +16,53 @@ BEGIN {
 use Moose::Role;
 use namespace::autoclean;
 
+has config => (
+	is       => 'rw',
+	isa      => 'HashRef[Str]',
+	traits   => ['Hash'],
+	required => 1,
+);
+
+sub submit {
+	my $self = shift;
+
+	my $content = { $self->content };
+
+	my $month = substr( $content->{expiration}, 0, 2 );
+	my $year  = substr( $content->{expiration}, 2, 2 );
+
+	my $checkout = Checkout::CyberSource::SOAP->new(
+		id         => $content->{login},
+		key        => $content->{password},
+		production => $content->{production} //= 0,
+		column_map => {
+			firstName       => $content->{first_name},
+			lastName        => $content->{last_name},
+			street1         => $content->{address},
+			city            => $content->{city},
+			state           => $content->{state},
+			zip             => $content->{zip},
+			country         => $content->{country},
+			email           => $content->{email},
+			ipAddress       => $content->{customer_ip},
+			accountNumber   => $content->{card_number},
+			expirationMonth => $month,
+			expirationYear  => $year,
+			unitPrice       => $content->{amount},
+		},
+    );
+
+	my $response = $checkout->checkout;
+
+	if ( $response->success ) {
+		$self->is_success(1);
+	}
+	else {
+		$self->error_message( $response->error->{message} );
+	}
+	return 1;
+}
+
 1;
 
 # ABSTRACT: Checkout::CyberSource::SOAP backend for Business::OnlinePayment::Cybersource
